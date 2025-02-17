a2d538821c06949b6c0422ac182dd65aff6a8640
sai shandilya <bathula.shandilya@gmail.com>
2025-02-17 22:47:22 +0530

    feat: adding the Jenkins File

--- eks-infra-using-pipeline/provider.tf
+++ eks-infra-using-pipeline/provider.tf
@@ -1,14 +1,12 @@
-/*
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.11.0"
+      version = "~> 5.0"
     }
   }
 }
-*/
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
